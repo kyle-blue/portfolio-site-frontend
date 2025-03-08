e53c0b73fe0496ae62066ef0c6df506e4ba3ac77
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2025-03-08 20:31:12 +0000

    Added ui for home landing section
    Including top navigation bar, background layered gradients, fonts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
+const formattedDate = new Intl.DateTimeFormat('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: '2-digit',
+}).format(new Date())
+
 /** @type {import('vite').UserConfig} */
 const config = ({ mode }) => {
     return defineConfig({
+        define: {
+            env: {
+                LAST_UPDATED: formattedDate,
+            },
+        },
         plugins: [
             react(),
             svgr({
