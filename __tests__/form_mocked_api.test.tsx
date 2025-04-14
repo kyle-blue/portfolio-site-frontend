9c753a3d7cf62b83066b3c40865e005498252f4b
Kyle Doidge <kyle.blue.doidge@gmail.com>
2025-04-14 19:09:24 +0100

    Add faked api helper version of contact form unit tests

--- __tests__/form_mocked_api.test.tsx
+++ __tests__/form_mocked_api.test.tsx
@@ -1,16 +1,10 @@
-import { describe, test, afterEach, vi, expect } from 'vitest'
+import { describe, test, beforeAll, afterAll, afterEach, vi, expect } from 'vitest'
+import mockServer from './mocks/form_server'
 import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import ContactForm from '../src/pages/Root/ContactForm'
 import React from 'react'
-import { sendEmail } from '../src/api/email'
-
-vi.mock('../src/api/email', async (importOriginal) => {
-    const original = await importOriginal()
-    return {
-        ...(original as any),
-        sendEmail: vi.fn(() => ({ status: 200, headers: {}, body: { message: 'success' } })),
-    }
-})
+import { send_email_url } from './mocks/form_handlers'
+import { http, HttpResponse } from 'msw'
 
 function submitMockContactForm() {
     const nameInput = screen.getByRole('textbox', { name: /name/i })
@@ -29,39 +23,58 @@ function submitMockContactForm() {
     fireEvent.click(submitButton)
 }
 
+function getEmailEndpointMock() {
+    const emailMock = vi.fn()
+    mockServer.events.on('request:start', ({ request }) => {
+        if (request.url === send_email_url && request.method === 'POST') {
+            emailMock(request)
+        }
+    })
+    return emailMock
+}
+
 describe('Contact form', () => {
-    test('Fake Helpers: Displays success on successful form submit', async () => {
-        // vi.mocked is just used for more complete ts typing
-        const sendEmailMock = vi.mocked(sendEmail)
+    beforeAll(() => {
+        mockServer.listen({ onUnhandledRequest: 'error' })
+    })
+
+    test('Mock Api: Displays success on successful form submit', async () => {
         render(<ContactForm />)
 
+        const emailMock = getEmailEndpointMock()
         submitMockContactForm()
-        expect(sendEmailMock).toHaveBeenCalledOnce()
+        expect(emailMock).toHaveBeenCalledOnce()
 
         await waitFor(() => {
             const snackBar = screen.queryByRole('snackbar')
             expect(snackBar?.textContent?.toLocaleLowerCase()).includes('email successfully sent')
         })
     })
-    test('Fake Helpers: Displays error on Api Error', async () => {
-        const sendEmailMock = vi.mocked(sendEmail)
+    test('Mock Api: Displays error on Api Error', async () => {
+        // Error out on send email url
+        mockServer.use(
+            http.post(send_email_url, () => {
+                return HttpResponse.json({}, { status: 500 })
+            }),
+        )
+
         render(<ContactForm />)
 
-        sendEmailMock.mockResolvedValueOnce({ status: 400, headers: {}, body: { message: '' } })
-
+        const emailMock = getEmailEndpointMock()
         submitMockContactForm()
-        expect(sendEmailMock).toHaveBeenCalledOnce()
+        expect(emailMock).toHaveBeenCalledOnce()
 
         await waitFor(() => {
             const snackBar = screen.queryByRole('snackbar')
             expect(snackBar?.textContent?.toLocaleLowerCase()).toContain('request failed')
         })
     })
-
-    test('Displays error on lack of form data', () => {})
-
     afterEach(() => {
+        mockServer.resetHandlers()
         cleanup()
         vi.clearAllMocks()
+    })
+    afterAll(() => {
+        mockServer.close()
     })
 })
